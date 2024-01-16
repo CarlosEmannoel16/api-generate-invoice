54ee351646908ede3e5533a4a9acb4e4a0cd0756
Carlos Emannoel <carlosemannoel2019@gmail.com>
2024-01-16 20:55:46 -0300

    feat: estruruta do pdf

--- src/GenaratedService.ts
+++ src/GenaratedService.ts
@@ -14,106 +14,133 @@ export type GeneratedBookletParams = {
 
 export class GeneratedBookletService {
   private docDefinition: {
+    pageOrientation: string;
     content: any[];
   };
 
   constructor() {
     this.docDefinition = {
       content: [],
+      pageOrientation: "landscape",
     };
 
     pdfMake.vfs = pdfFonts.pdfMake.vfs;
   }
 
   async generateBooklet(params: GeneratedBookletParams) {
-    this.docDefinition.content = [
-        { text: "Carne de Pagamento", fontSize: 18, bold: true, alignment: 'center' },
-        { text: "Telefone para contato", fontSize: 12, alignment: 'center', margin:[0,0,0,10] },
-            {
-              style: 'tableExample',
-          
-              table: {
-                 widths: [50, 100, 150, 150],
-                  headerRows: 1,
-                  body: [
-                      [
-                          [
-                              {text: 'Parcela', alignment: 'center', border: [true]}, 
-                              {text: '1', alignment: 'center', bold: true}
-                         ],
-              
-      
-                         [
-                             {text: 'Vencimento', alignment: 'center', border: [true] }, 
-                             {text: '01/02/2023', alignment: 'center', bold: true}
-                         ], 
-                         
-                         [ {text: 'Local de Pagamento', alignment: 'center', border: [true]}],
-                         [
-                             {text: 'Vencimento', alignment: 'center', border: [true] }, 
-                             {text: '01/02/2023', alignment: 'center', bold: true}
-                         ],
-                         
-                         
-                    ],
-                    
+    this.docDefinition = {
+    pageOrientation: 'landscape',
+    content:[
+          { text: "Carnê de Pagamento", fontSize: 18, bold: true, alignment: 'center' },
+          { text: "Telefone para contato", fontSize: 12, alignment: 'center', margin:[0,0,0,10] },
+              {
+                style: 'tableExample',
+            
+                table: {
+                   widths: [50, 100, 250, 150],
+                    headerRows: 1,
+                    body: [
                         [
-                       {
-                           colSpan: 2,
-                           text: 'Valor do documento \n R$ 3.345,90',
-                           alignment: 'center'
-                       },
-                        {
+                            [
+                                {text: 'Parcela', alignment: 'center', border: [true]}, 
+                                {text: '1', alignment: 'center', bold: true}
+                           ],
+                
+        
+                           [
+                               {text: 'Vencimento', alignment: 'center', border: [true] }, 
+                               {text: '01/02/2023', alignment: 'center', bold: true}
+                           ], 
                            
-                        },
-                        [{text: 'Cedente', alignment: 'center', border: [true] }],
-                         [
-                              {text: '(=) Valor do documento', alignment: 'center', border: [true] }, 
-                             {text: '01/02/2023', alignment: 'center', bold: true}
-                         ],
-                         
-                         
-                    ],
+                           [ {text: 'Local de Pagamento', alignment: 'center', border: [true]}],
+                           [
+                               {text: 'Vencimento', alignment: 'center', border: [true] }, 
+                               {text: '01/02/2023', alignment: 'center', bold: true}
+                           ],
+                           
+                           
+                      ],
+                        [
+                         {
+                             colSpan: 2,
+                             text: 'Valor do documento \n R$ 3.345,90',
+                             alignment: 'center'
+                         },
+                          {
+                             
+                          },
+                          [{text: 'Cedente', alignment: 'center', border: [true] }],
+                           [
+                                {text: '(=) Valor do documento', alignment: 'center', border: [true] }, 
+                               {text: '01/02/2023', alignment: 'center', bold: true}
+                           ],
+                           
+                           
+                      ],
                       
-                  ]
-              },
-          
-                  layout: {
-                  hLineWidth: function (i, node) {
-                      if(i==0) return 2
-                      return 1
-                  },
-                  vLineWidth: function (i, node) {
-                      return 1
-                  },
-                  hLineColor: function (i, node) {
-                      return 'black';
-                  },
-                  vLineColor: function (i, node) {
-                      return 'black';
-                  },
-                  hLineStyle: function (i, node) {
-                      if (i === 0 || i === node.table.body.length) {
-                          return null;
-                      }
+                        [
+                            [
+                                {text: 'Parcela', alignment: 'center', border: [true]}, 
+                                {text: '1', alignment: 'center', bold: true}
+                           ],
+                
+        
+                           [
+                               {text: 'Vencimento', alignment: 'center', border: [true] }, 
+                               {text: '01/02/2023', alignment: 'center', bold: true}
+                           ], 
+                           
+                           [ {text: 'Local de Pagamento', alignment: 'center', border: [true]}],
+                           [
+                               {text: '(-) Desconto', alignment: 'center', border: [true] }, 
+                               {text: 'R$ 12,90', alignment: 'center', bold: true}
+                           ],
+                           
+                           
+                      ],
                       
-                      return {dash: {length: 10, space: 1}};
-                  },
-                  vLineStyle: function (i, node) {
-                      if (i === 0 || i === node.table.widths.length) {
-                          return null;
-                      }
-                      return {dash: {length: 4}};
-                  },
-                  paddingLeft: function(i, node) { return 0; },
-                  // paddingRight: function(i, node) { return 4; },
-                  paddingTop: function(i, node) { return 0; },
-                  // paddingBottom: function(i, node) { return 2; },
-                  // fillColor: function (i, node) { return null; }
-              }
-          },
-  
-      ]
+                        
+                    
+                    ]
+                },
+            
+                    layout: {
+                    hLineWidth: function (i, node) {
+                        if(i==0) return 2
+                        return 1
+                    },
+                    vLineWidth: function (i, node) {
+                        return 1
+                    },
+                    hLineColor: function (i, node) {
+                        return 'black';
+                    },
+                    vLineColor: function (i, node) {
+                        return 'black';
+                    },
+                    hLineStyle: function (i, node) {
+                        if (i === 0 || i === node.table.body.length) {
+                            return null;
+                        }
+                        
+                        return {dash: {length: 10, space: 1}};
+                    },
+                    vLineStyle: function (i, node) {
+                        if (i === 0 || i === node.table.widths.length) {
+                            return null;
+                        }
+                        return {dash: {length: 4}};
+                    },
+                    paddingLeft: function(i, node) { return 0; },
+                    // paddingRight: function(i, node) { return 4; },
+                    paddingTop: function(i, node) { return 0; },
+                    // paddingBottom: function(i, node) { return 2; },
+                    // fillColor: function (i, node) { return null; }
+                }
+            },
+    
+        ]
+  }
 
     const pdfData = pdfMake.createPdf(this.docDefinition);
 
